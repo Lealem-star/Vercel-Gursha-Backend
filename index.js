8c20d0934fc3feb37659ae556d8fcec7e46ee384
Lealem Meseret <meseretlealem8@gmail.com>
2025-07-27 09:11:00 +0300

    update cors again

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'https://vercel-gursha-frontend.vercel.app/',
+    origin: 'https://vercel-gursha-frontend.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version']  //to be changed later to vercel url
