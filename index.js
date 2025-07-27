7786be1c8667c168686f16663447b32a50f99f02
Lealem Meseret <meseretlealem8@gmail.com>
2025-07-27 09:14:49 +0300

    update again and again cors

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ if (!fs.existsSync(uploadsDir)) {
 }
 
 // Middleware
-app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
