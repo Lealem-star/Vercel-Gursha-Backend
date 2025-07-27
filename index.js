1a224478fd5f4ea4d6e6d45a9065ed645db3bb2b
Lealem Meseret <meseretlealem8@gmail.com>
2025-07-27 09:02:41 +0300

    edit vercel

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'https://vercel-gursha-frontend.vercel.app',  //to be changed later to vercel url
+    origin: 'https://vercel-gursha-frontend.vercel.app/',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version']
+    allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version']  //to be changed later to vercel url
 }));
 
 // Create uploads directory if it doesn't exist
@@ -29,6 +29,7 @@ if (!fs.existsSync(uploadsDir)) {
 }
 
 // Middleware
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
